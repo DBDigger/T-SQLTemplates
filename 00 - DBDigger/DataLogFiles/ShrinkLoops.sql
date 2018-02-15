896f25847f6be6a6d2240bd17094bbac009c5133
Atif Shehzad <ashehzad@asicentral.com>
2018-02-15 09:22:00 -0500

    Update shrink loop script

--- 00 - DBDigger/DataLogFiles/ShrinkLoops.sql
+++ 00 - DBDigger/DataLogFiles/ShrinkLoops.sql
@@ -1,19 +1,23 @@
--- get data file space and locations
-SELECT 'declare @filesize int;'+'set @filesize = '+CAST(CONVERT(INT, a.Size / 128.000, 2)+500 as VARCHAR(10))+
-'; WHILE @filesize > '+cast(CONVERT(INT, a.Size / 128.000, 2)+1024 as varchar(10))+
-'; BEGIN; DBCC SHRINKFILE (N'''+a.NAME+''' , @filesize);  WAITFOR DELAY ''00:00:07'';  SET @filesize = @filesize - 1000; END GO' as ShrinkLoop,
-b.groupname AS 'File Group'
-	,a.NAME
-	,physical_name
-	,CONVERT(INT, a.Size / 128.000, 2) AS [Currently Allocated Space (MB)]
-	,CONVERT(INT, FILEPROPERTY(a.NAME, 'SpaceUsed') / 128.000, 2) AS [Space Used (MB)]
-	,CONVERT(INT, (a.Size - FILEPROPERTY(a.NAME, 'SpaceUsed')) / 128.000, 2) AS [Available Space (MB)]
-	,(CONVERT(INT, ROUND((a.Size - FILEPROPERTY(a.NAME, 'SpaceUsed')) / 128.000, 2)) * 100) / (CONVERT(INT, ROUND(a.Size / 128.000, 2))) AS PercentFree
-FROM sys.database_files a(NOLOCK)
-LEFT OUTER JOIN sysfilegroups b(NOLOCK) ON a.data_space_id = b.groupid
+SELECT
+  'declare @filesize int;' + 'set @filesize = ' + CAST(CONVERT(int, a.Size / 128.000, 2) 
+  + 500 AS varchar(10)) + '; WHILE @filesize > ' + CAST(CONVERT(int, a.Size / 128.000, 2) 
+  + 1024 AS varchar(10)) + ' BEGIN; DBCC SHRINKFILE (N''' + a.NAME + 
+  ''' , @filesize);  WAITFOR DELAY ''00:00:07'';  SET @filesize = @filesize - 1000; END GO' AS ShrinkLoop,
+  b.groupname AS 'File Group',
+  a.NAME,
+  physical_name,
+  CONVERT(int, a.Size / 128.000, 2) AS [Currently Allocated Space (MB)],
+  CONVERT(int, FILEPROPERTY(a.NAME, 'SpaceUsed') / 128.000, 2) AS [Space Used (MB)],
+  CONVERT(int, (a.Size - FILEPROPERTY(a.NAME, 'SpaceUsed')) / 128.000, 2) AS [Available Space (MB)],
+  (CONVERT(int, ROUND((a.Size - FILEPROPERTY(a.NAME, 'SpaceUsed')) / 128.000, 2)) * 100) / (CONVERT(int, ROUND(a.Size / 128.000, 2))) AS PercentFree
+FROM sys.database_files a (NOLOCK)
+LEFT OUTER JOIN sysfilegroups b (NOLOCK)
+  ON a.data_space_id = b.groupid
 --where (CONVERT(INT, ROUND((a.Size - FILEPROPERTY(a.NAME, 'SpaceUsed')) / 128.000, 2)) * 100) / (CONVERT(INT, ROUND(a.Size / 128.000, 2))) > 30
 --where CONVERT(INT, (a.Size - FILEPROPERTY(a.NAME, 'SpaceUsed')) / 128.000, 2) > 10000
-ORDER BY 6 desc
+--where physical_name like 'F%'
+ORDER BY [Available Space (MB)] DESC
+GO
 
 
 
